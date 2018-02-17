0cae43260c43f8e550b836d030f560145f6ded2d
Andrei Cristian Petcu <andrei@ceata.org>
2018-02-17 14:29:15 +0200

    first certificate test, failing

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -3,8 +3,24 @@
 
 'use strict';
 
+function str2ab(str) {
+    var buf = new ArrayBuffer(str.length);
+    var bufView = new Uint8Array(buf);
+
+    for(var i = 0, strLen = str.length; i < strLen; i++)
+        bufView[i] = str.charCodeAt(i);
+
+    return buf;
+};
+
 function getCertificate(source) {
-  return source;
+  var CertPEM = source.replace(/(-----(BEGIN|END) CERTIFICATE-----|\n)/g, '');
+  var CertBuf = str2ab(CertPEM);
+  var asn1 = pkijs.org.pkijs.fromBER(CertBuf);
+  var cert_simpl = new pkijs.org.pkijs.simpl.CERT({ schema: asn1.result });
+  return {
+    commonName: 'fake'
+  };
 }
 
 chrome.runtime.onInstalled.addListener(function (details) {
