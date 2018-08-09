1dfdc5a5785221582523a25081d7005f8cedd658
Andrei Cristian Petcu <andrei@ceata.org>
2018-08-09 19:53:02 +0300

    added test for fields

--- app/pages/certificate_details.js
+++ app/pages/certificate_details.js
@@ -1,39 +1,6 @@
-// function getWindowLocation() {
-//   // if (isInsideRealBrowser()) {
-//       return window.location;
-// //   } else {
-// //       return window.privacyLabradorLocation.location;
-// //   }
-// }
-
-
-// console.log(getWindowLocation());
-// // content-script.js
-// function handleResponse(message) {
-//   console.log(`background script sent a response: ${message.response}`);
-// }
-
-// function handleError(error) {
-//   console.log(`Error: ${error}`);
-// }
-
-// function sendMessage() {
-//   console.log("sending message from content");
-//   var sending = chrome.runtime.sendMessage({
-//     content: "message from the content script"
-//   });
-//   if(sending){
-//     sending.then(handleResponse, handleError);
-//   }
-// }
-
-// sendMessage();
-
-function initCertDetails() {
-  console.log('Initializing cert details');
-  document.querySelector('h1').textContent = 'Certificate details';
+function populateCertificate(certificateData){
 }
 
 if (global) {
-  global.initCertDetails = initCertDetails;
+  global.populateCertificate = populateCertificate;
 }
--- test/spec/test.js
+++ test/spec/test.js
@@ -39,7 +39,7 @@
 
       it('should have valid sha256 fingerprint', function () {
         expect(getCertificate(getLetsEncryptCertificate()).sha256fingerprint)
-            .toBe('0687260331A72403D909F105E69BCF0D32E1BD2493FFC6D9206D11BCD6770739');
+          .toBe('0687260331A72403D909F105E69BCF0D32E1BD2493FFC6D9206D11BCD6770739');
       });
 
       it('should have nice formated representation', function () {
@@ -81,12 +81,31 @@
     });
 
     describe('opening certificate details page', function () {
-      it('should have the correct title', function () {
+      it('should have the correct fields', function () {
         document.body.innerHTML = window.__html__['app/pages/certificate_details.html'];
 
-        initCertDetails();
+        populateCertificate({
+          commonName: "DST Root CA X3",
+          organization: "Digital Signature Trust Co.",
+          serialNumber: "44afb080d6a327ba893039862ef8406b",
+          issuer: "DST Root CA X3, Digital Signature Trust Co.",
+          validFrom: "30 September 2000 [2000-09-30T21:12:19.000Z]",
+          validTo: "30 September 2021 [2021-09-30T14:01:15.000Z]",
+          sha1fingerprint: "DAC9024F54D8F6DF94935FB1732638CA6AD77C13",
+          sha256fingerprint: "0687260331A72403D909F105E69BCF0D32E1BD2493FFC6D9206D11BCD6770739",
+        });
 
-        expect(document.querySelector('h1').textContent).toBe('Certificate details');
+        detailsListItems = document.querySelector('#content ul li');
+
+        expect(detailsListItems[0]).toBe('Common name: DST Root CA X3');
+        expect(detailsListItems[1]).toBe('Organization: Digital Signature Trust Co.');
+        expect(detailsListItems[2]).toBe('Issuer: DST Root CA X3, Digital Signature Trust Co.');
+        expect(detailsListItems[3]).toBe('Serial Number: 44afb080d6a327ba893039862ef8406b');
+        expect(detailsListItems[4]).toBe('Valid From: 30 September 2000 [2000-09-30T21:12:19.000Z]');
+        expect(detailsListItems[5]).toBe('Valid To: 30 September 2021 [2021-09-30T14:01:15.000Z]');
+        expect(detailsListItems[6]).toBe('SHA1: DAC9024F54D8F6DF94935FB1732638CA6AD77C13');
+        expect(detailsListItems[7]).toBe('SHA256: 0687260331A72403D909F105E69BCF0D32E1BD2493FFC6D9206D11BCD6770739');
+
       });
     });
   });
