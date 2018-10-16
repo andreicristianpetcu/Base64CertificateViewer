d537283f8e07d9a4c38a0e995b76236429245499
Andrei Cristian Petcu <andrei@ceata.org>
2018-10-16 23:53:18 +0200

    made the new build system not minify things

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -4,26 +4,13 @@ import {
   log,
   colors
 } from 'gulp-util'
-import gp_concat from 'gulp-concat'
-import named from 'vinyl-named'
-import webpack from 'webpack'
-import gulpWebpack from 'webpack-stream'
-import plumber from 'gulp-plumber'
 import livereload from 'gulp-livereload'
 import args from './lib/args'
 
 const ENV = args.production ? 'production' : 'development'
 
-gulp.task('scripts', (cb) => {
-  return gulp.src([
-      'app/scripts/forge.min.js',
-      'app/scripts/background.js'
-    ])
-    .pipe(plumber({
-      errorHandler() {}
-    }))
-    .pipe(named())
-    .pipe(gp_concat('concatenated_background.js'))
+gulp.task('scripts', () => {
+  return gulp.src(['app/scripts/**/*.js'])
     .pipe(gulp.dest(`dist/${args.vendor}/scripts`))
     .pipe(gulpif(args.watch, livereload()))
-});
+})
