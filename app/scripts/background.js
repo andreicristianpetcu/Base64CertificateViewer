c7d684b57f6cdb9e838b55ed3b414e9b42cfabf9
Andrei Cristian Petcu <andrei@ceata.org>
2019-01-26 18:10:59 +0100

    print cert from selection or clipboard

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,82 +1,82 @@
-var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
-function getAttributeValueByName(allAttributes, attributeName) {
-  var foundValue;
+function getAttributeValueByName (allAttributes, attributeName) {
+  var foundValue
   allAttributes.forEach(attribute => {
     if (attribute.name === attributeName) {
-      foundValue = attribute.value;
+      foundValue = attribute.value
     }
-  });
-  return foundValue;
+  })
+  return foundValue
 }
 
-function getUtcDate(date) {
-  return JSON.stringify(date).split('"')[1];
+function getUtcDate (date) {
+  return JSON.stringify(date).split('"')[1]
 }
 
-function getHumanReadableDate(utcDateAsString) {
-  const date = new Date(utcDateAsString);
-  return date.getUTCDate() + " " + months[date.getUTCMonth()] + " " + date.getUTCFullYear();
+function getHumanReadableDate (utcDateAsString) {
+  const date = new Date(utcDateAsString)
+  return date.getUTCDate() + ' ' + months[date.getUTCMonth()] + ' ' + date.getUTCFullYear()
 }
 
-function getCertificateFomXml(xmlContainingCertificate) {
-  var xmlTagRegex = /(<([^>]+)>)/ig;
-  xmlContainingCertificate = xmlContainingCertificate.replace(xmlTagRegex, "");
-  return xmlContainingCertificate;
+function getCertificateFomXml (xmlContainingCertificate) {
+  var xmlTagRegex = /(<([^>]+)>)/ig
+  xmlContainingCertificate = xmlContainingCertificate.replace(xmlTagRegex, '')
+  return xmlContainingCertificate
 }
 
-function stripIdenticalPrefixAndSufix(rawCertificate, prefixAndSufix){
-  if(rawCertificate.charAt(0) == prefixAndSufix && rawCertificate.charAt(rawCertificate.length - 1) == prefixAndSufix){
+function stripIdenticalPrefixAndSufix (rawCertificate, prefixAndSufix) {
+  if (rawCertificate.charAt(0) == prefixAndSufix && rawCertificate.charAt(rawCertificate.length - 1) == prefixAndSufix) {
     rawCertificate = rawCertificate.substring(1, rawCertificate.length - 1)
   }
-  return rawCertificate;
+  return rawCertificate
 }
 
-function cleanupCertificate(rawCertificate) {
-  rawCertificate = stripIdenticalPrefixAndSufix(rawCertificate, "\"");
-  rawCertificate = stripIdenticalPrefixAndSufix(rawCertificate, "\'");
+function cleanupCertificate (rawCertificate) {
+  rawCertificate = stripIdenticalPrefixAndSufix(rawCertificate, '"')
+  rawCertificate = stripIdenticalPrefixAndSufix(rawCertificate, '\'')
 
-  if (rawCertificate.indexOf("</") > -1 && rawCertificate.indexOf(">") > -1) {
-    rawCertificate = getCertificateFomXml(rawCertificate);
+  if (rawCertificate.indexOf('</') > -1 && rawCertificate.indexOf('>') > -1) {
+    rawCertificate = getCertificateFomXml(rawCertificate)
   }
 
-  if (rawCertificate.indexOf("+    ") > -1 || rawCertificate.indexOf("-    ") > -1) {
-    rawCertificate = rawCertificate.replace(/((\+|-)    )/g, '');
+  if (rawCertificate.indexOf('+    ') > -1 || rawCertificate.indexOf('-    ') > -1) {
+    rawCertificate = rawCertificate.replace(/((\+|-)    )/g, '')
   }
 
-  rawCertificate = rawCertificate.replace(/(-----(BEGIN|END) CERTIFICATE-----|\n)/g, '');
-  rawCertificate = '-----BEGIN CERTIFICATE-----\n' + rawCertificate + '\n-----END CERTIFICATE-----';
-  return rawCertificate;
+  rawCertificate = rawCertificate.replace(/(-----(BEGIN|END) CERTIFICATE-----|\n)/g, '')
+  rawCertificate = '-----BEGIN CERTIFICATE-----\n' + rawCertificate + '\n-----END CERTIFICATE-----'
+  return rawCertificate
 }
 
-function getFullFormatedDate(dateAsString) {
-  return getHumanReadableDate(getUtcDate(dateAsString)) + " [" + getUtcDate(dateAsString) + "]";
+function getFullFormatedDate (dateAsString) {
+  return getHumanReadableDate(getUtcDate(dateAsString)) + ' [' + getUtcDate(dateAsString) + ']'
 }
 
-function getShaFingerprint(myCertificate, hashAlgorithm) {
-  var asn1 = forge.pki.certificateToAsn1(myCertificate);
-  var der = forge.asn1.toDer(asn1);
-  var md = forge.md[hashAlgorithm].create();
-  md.update(der.data);
-  return md.digest().toHex().toUpperCase();
+function getShaFingerprint (myCertificate, hashAlgorithm) {
+  var asn1 = forge.pki.certificateToAsn1(myCertificate)
+  var der = forge.asn1.toDer(asn1)
+  var md = forge.md[hashAlgorithm].create()
+  md.update(der.data)
+  return md.digest().toHex().toUpperCase()
 }
 
-function getCertificate(source) {
-  const base64Cert = cleanupCertificate(source);
+function getCertificate (source) {
+  const base64Cert = cleanupCertificate(source)
 
-  const caStore = forge.pki.createCaStore([base64Cert]);
+  const caStore = forge.pki.createCaStore([base64Cert])
 
-  const myCertificate = caStore.listAllCertificates()[0];
+  const myCertificate = caStore.listAllCertificates()[0]
 
-  const subjectCommonName = getAttributeValueByName(myCertificate.subject.attributes, 'commonName');
-  const subjectOrganization = getAttributeValueByName(myCertificate.subject.attributes, 'organizationName');
-  const issuerCommonName = getAttributeValueByName(myCertificate.issuer.attributes, 'commonName');
-  const issuerOrganization = getAttributeValueByName(myCertificate.issuer.attributes, 'organizationName');
-  const issuer = issuerCommonName + ', ' + issuerOrganization;
-  const validFrom = getFullFormatedDate(myCertificate.validity.notBefore);
-  const validTo = getFullFormatedDate(myCertificate.validity.notAfter);
-  const sha1fingerprint = getShaFingerprint(myCertificate, 'sha1');
-  const sha256fingerprint = getShaFingerprint(myCertificate, 'sha256');
+  const subjectCommonName = getAttributeValueByName(myCertificate.subject.attributes, 'commonName')
+  const subjectOrganization = getAttributeValueByName(myCertificate.subject.attributes, 'organizationName')
+  const issuerCommonName = getAttributeValueByName(myCertificate.issuer.attributes, 'commonName')
+  const issuerOrganization = getAttributeValueByName(myCertificate.issuer.attributes, 'organizationName')
+  const issuer = issuerCommonName + ', ' + issuerOrganization
+  const validFrom = getFullFormatedDate(myCertificate.validity.notBefore)
+  const validTo = getFullFormatedDate(myCertificate.validity.notAfter)
+  const sha1fingerprint = getShaFingerprint(myCertificate, 'sha1')
+  const sha256fingerprint = getShaFingerprint(myCertificate, 'sha256')
 
   const certData = {
     commonName: subjectCommonName,
@@ -95,38 +95,45 @@ Serial Number: ${myCertificate.serialNumber}
 Valid From: ${validFrom}
 Valid To: ${validTo}
 SHA1: ${sha1fingerprint}
-SHA256: ${sha256fingerprint}`;
+SHA256: ${sha256fingerprint}`
     }
-  };
+  }
 
-  return certData;
+  return certData
 }
 
 chrome.contextMenus.create({
   id: 'view-certificate',
-  title: 'Print certificate contents',
-  contexts: ['selection']
-});
+  title: 'Print certificate contents'
+})
 
-var selectionText;
+var selectionText
 
-function handleMessage(request, sender, sendResponse) {
-  const certificateData = getCertificate(selectionText);
-  sendResponse(certificateData);
+function handleMessage (request, sender, sendResponse) {
+  const certificateData = getCertificate(selectionText)
+  sendResponse(certificateData)
 }
-chrome.runtime.onMessage.addListener(handleMessage);
+
+chrome.runtime.onMessage.addListener(handleMessage)
 
 chrome.contextMenus.onClicked.addListener(function (info) {
   if (info.menuItemId === 'view-certificate') {
-    selectionText = info.selectionText;
 
-    chrome.tabs.create({
-      url: `/pages/certificate_details.html`,
-    });
+    navigator.clipboard.readText().then(function (textFromClipboard) {
+      if (info.selectionText) {
+        selectionText = info.selectionText
+      } else {
+        selectionText = textFromClipboard
+      }
+      chrome.tabs.create({
+        url: `/pages/certificate_details.html`
+      })
+
+    })
 
   }
-});
+})
 
-if (typeof global !== "undefined") {
-  global.getCertificate = getCertificate;
+if (typeof global !== 'undefined') {
+  global.getCertificate = getCertificate
 }
